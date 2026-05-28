321e12356b3baa327caa6d3c874e0355d2104edd
aashumalik784 <aashumalik784@gmail.com>
2026-05-28 16:58:34 +0530

    App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{padding: "20px", fontFamily: "sans-serif"}}>
+      <h1>OTT Cinema 🎬</h1>
+      <p>Project setup done! Ab hum UI banayenge.</p>
+    </div>
+  )
+}
+
+export default App
